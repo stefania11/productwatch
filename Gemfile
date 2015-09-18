248bb19f7e06e0ea822b846b16aba057f122dfdd
Tyler Machen <tylermachen@gmail.com>
2015-09-18 15:54:06 -0400

    comment out initialize_on_precompile

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '2.2.0'
+
 # include .env files
 gem 'dotenv-rails', :groups => [:development, :test]
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.initialize_on_precompile
+# Rails.application.config.assets.initialize_on_precompile
