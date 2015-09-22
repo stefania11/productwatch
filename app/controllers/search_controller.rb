766abd35d062360fbcb07e966e3cab8257198097
Gerardo Veltri <gerardo.veltri@gmail.com>
2015-09-22 17:12:14 -0400

    made a function in the search controller that finds the best and worst reviews and sentences from all reviews that are being shown.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,9 +9,31 @@ class SearchController < ApplicationController
     #shows the review based on what the user clicks
     @product = params[:product]
     @product[:reviews] = ReviewParser.call(params[:product][:id])
+    @dasbhoard = dashboard_text
   end
 
   private
+
+  def dashboard_text
+    best_sentence = @product[:reviews].first
+    worst_sentence = @product[:reviews].first
+    best_review = @product[:reviews].first
+    worst_review = @product[:reviews].first
+
+    @product[:reviews].each do |review|
+      best_sentence = review if review.high_score > best_sentence.high_score
+      worst_sentence = review if review.low_score < worst_sentence.low_score
+      best_review = review if review.overall_score > best_review.overall_score
+      worst_review = review if review.overall_score < worst_review.overall_score
+    end
+    
+    {
+      best_sentence: best_sentence.high_sentence,
+      worst_sentence: worst_sentence.low_sentence,
+      best_review: best_review.content,
+      worst_review: worst_review.content
+    }
+  end
 
   def item_params
     params.require(:product).permit(:id, :title, :image_url)
