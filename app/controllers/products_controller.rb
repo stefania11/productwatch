b77da2e77ffd9157b02a647282381e0cbc82afb4
stefania11 <druga.stefania@gmail.com>
2015-09-23 17:16:07 -0400

    add working map

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,15 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @reviews = ReviewParser.call(@product.asin, @product.id)
-    @map_data = build_map_data
-    binding.pry
+    # @reviews = []
+     @map_data = build_map_data
+    # @map_data = {
+    #   AL: 0.03333333333333333,
+    #   AR: 0.16666666666666666,
+    #   CO: 0.16666666666666666,
+    #   IN: 0.03333333333333333,
+    #   NY: 0.16666666666666666
+    # }
     @dashboard = dashboard_text
   end
 
@@ -35,15 +42,16 @@ class ProductsController < ApplicationController
   end
 
   def build_map_data
-    h = Hash.new(0)
+    rating_hash = Hash.new(0)
+    count_hash = Hash.new(0)
+
     @reviews.each do |review|
-      # h[review.author.location] = average
-      review.author.map do |author|
-        review.rating.first.to_i
-      end
-      # h[review.author.location].map { |k, v| k,v.to_s }
-      # h[review.author.location] = h[review.author.location].reduce(:+)
+      rating_hash[review.author.location] += review.rating.first.to_f
+      count_hash[review.author.location] += 1
     end
+    rating_hash.each { |k, v| rating_hash[k] = (v / count_hash[k]) / 5 }
+    return rating_hash
+
   end
 
   def item_params
