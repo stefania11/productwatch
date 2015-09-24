49ff8aa11666380d500c9577b3a7886a3ca80515
Tyler Machen <tylermachen@gmail.com>
2015-09-24 15:55:46 -0400

    add areaspline graph to dashboard

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,19 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @reviews = ReviewParser.call(@product.asin, @product.id)
-    @map_data = build_map_data
+    # @map_data = build_map_data
     @dashboard = dashboard_text
+
+    # chart stuff
+    @graph_data = get_graph_data
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.chart(type: 'areaspline')
+      f.title(text: 'Average Rating Over Time')
+      f.xAxis(categories: @graph_data[:years])
+      f.yAxis(title: {text: 'Rating', margin: 50})
+      f.series(name: 'Average Rating', data: @graph_data[:ratings])
+      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+    end
   end
 
   def author_data
@@ -40,6 +51,26 @@ class ProductsController < ApplicationController
     h = Hash.new(0)
     @reviews.each { |r| h[r.author.location] += r.rating.first.to_f }
     h.each { |k, v| h[k] = v / 30 }
+  end
+
+  def get_graph_data
+    avg_rtg_per_year = Hash.new(0)
+    num_rtg_per_year = Hash.new(0)
+    formatted_data = {years: [], ratings: []}
+
+    @reviews.each { |r| avg_rtg_per_year[r.date.year] += r.rating.first.to_f }
+    @reviews.each { |r| num_rtg_per_year[r.date.year] += 1 }
+
+    avg_rtg_per_year.each do |k, v|
+      avg_rtg_per_year[k] = (v / num_rtg_per_year[k].to_f).round(2)
+    end
+
+    avg_rtg_per_year.sort.each do |r|
+      formatted_data[:years] << r.first
+      formatted_data[:ratings] << r.last
+    end
+
+    formatted_data
   end
 
   def item_params
--- db/migrate/20150922174632_create_authors.rb
+++ db/migrate/20150922174632_create_authors.rb
@@ -1,8 +1,6 @@
 class CreateAuthors < ActiveRecord::Migration
   def change
     create_table :authors do |t|
-      t.string :name
-      t.string :url
       t.string :location
 
       t.timestamps null: false
