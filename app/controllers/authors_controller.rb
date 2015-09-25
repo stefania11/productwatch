b21189970edb7e7a3986e3a2babbf9fde9a5b936
Gerardo Veltri <gerardo.veltri@gmail.com>
2015-09-25 14:30:31 -0400

    worked out bugs in the ajax, put in a loading gif, etc. etc.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,36 +11,38 @@ class AuthorsController < ApplicationController
 
   def authors_to_json(params)
     map_data = []
-    params["urls"].each_with_index do |url,i|
-      review = Review.find(params["ids"][i])
-      author = Author.where(url: url)[0]
-      info = {}
-      if author != nil
-        
-        info = {
-          name: author.name,
-          url: author.url,
-          location: author.location
+    if params["urls"]
+      params["urls"].each_with_index do |url,i|
+        review = Review.find(params["ids"][i])
+        author = Author.where(url: url)[0]
+        info = {}
+        if author != nil
+          
+          info = {
+            name: author.name,
+            url: author.url,
+            location: author.location
+          }
+          
+        else
+          loc_name = ProfileParser.get_author_location_and_name(url)
+          info = {
+            name: loc_name[1],
+            url: url,
+            location: loc_name[0]
+          }
+          review.create_author(info)
+          
+        end
+        map_datum = {
+          rating: review.rating[0...2].to_f,
+          state_abbr: info[:location]
         }
-        
-      else
-        loc_name = ProfileParser.get_author_location_and_name(url)
-        info = {
-          name: loc_name[1],
-          url: url,
-          location: loc_name[0]
-        }
-        review.create_author(info)
+        map_data << map_datum
         
       end
-      map_datum = {
-        rating: review.rating[0...2].to_f,
-        state_abbr: info[:location]
-      }
-      map_data << map_datum
-      
     end
-    map_data
+    return map_data
   end
   
 end
